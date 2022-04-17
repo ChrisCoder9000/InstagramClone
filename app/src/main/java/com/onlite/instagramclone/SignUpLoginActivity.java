e30b3dfc9097ffb2c15ca39022f2d053fb1e0a0d
ChrisCoder9000 <alch.infoemail@gmail.com>
2022-04-17 12:47:47 +0200

    SignUp and Login

--- app/src/main/java/com/onlite/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/onlite/instagramclone/SignUpLoginActivity.java
@@ -1,4 +1,65 @@
 package com.onlite.instagramclone;
 
-public class SignUpLoginActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    private EditText edtUserNameSignUp, edtPasswordSignUp, edtUserNameLogin, edtPasswordLogin;
+    private Button btnSignUp, btnLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_login_activity);
+
+        edtUserNameSignUp = findViewById(R.id.edtUserNameSignUp);
+        edtPasswordSignUp = findViewById(R.id.edtPasswordSignUp);
+        edtUserNameLogin = findViewById(R.id.edtUserNameLogin);
+        edtPasswordLogin = findViewById(R.id.edtPasswordLogin);
+
+        btnSignUp = findViewById(R.id.btnSignup);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnSignUp.setOnClickListener(v -> {
+            ParseUser appUser = new ParseUser();
+            appUser.setUsername(edtUserNameSignUp.getText().toString());
+            appUser.setPassword(edtPasswordSignUp.getText().toString());
+
+            appUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        FancyToast.makeText(SignUpLoginActivity.this, appUser.getUsername() + " is sign up!", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                    } else {
+                        FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                    }
+                }
+            });
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            ParseUser.logInInBackground(edtUserNameLogin.getText().toString(), edtPasswordLogin.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null) {
+                        FancyToast.makeText(SignUpLoginActivity.this, user.getUsername() + " is sign up!", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                    } else {
+                        FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                    }
+                }
+            });
+        });
+    }
 }
